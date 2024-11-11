32dbffda00720b2e523607cf8f7b4322dee66f0a
Dominik Gallitzendörfer <hello@nharox.com>
2024-11-11 17:31:36 +0100

    Define links

---
+++ src/data/links.ts
@@ -0,0 +1,33 @@
+interface Link {
+  href: string;
+  name: string;
+  iconName: string;
+}
+
+export const links: Link[] = [
+  {
+    href: 'https://nharox.com/',
+    name: 'Personal Website',
+    iconName: 'code',
+  },
+  {
+    href: 'https://nharox.games/',
+    name: 'Game Shelf',
+    iconName: 'joystick',
+  },
+  {
+    href: 'https://nharox.photos/',
+    name: 'Photos',
+    iconName: 'photo',
+  },
+  {
+    href: 'https://music.nharox.com/',
+    name: 'Favourite Music',
+    iconName: 'music',
+  },
+  {
+    href: 'https://mastodon.social/@nharox',
+    name: 'Mastodon',
+    iconName: 'mastodon',
+  },
+];
