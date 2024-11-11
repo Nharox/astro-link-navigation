0374a8a07764d9010944df69597669c214e2c366
Dominik Gallitzendörfer <hello@nharox.com>
2024-11-11 12:17:08 +0100

    Install and setup dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
+import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+});
